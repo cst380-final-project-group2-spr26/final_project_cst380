5bff347e39a280ce482fc599d7c0287c6dedf733
felipe-lopez-ordaz <felipelop470@gmail.com>
2026-04-18 13:59:27 -0700

    Add initial Xcode project setup

---
+++ SportsBuddyFinder/SportsBuddyFinder/SportsBuddyFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  SportsBuddyFinderApp.swift
+//  SportsBuddyFinder
+//
+//  Created by Felipe Lopez on 4/18/26.
+//
+
+import SwiftUI
+
+@main
+struct SportsBuddyFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
